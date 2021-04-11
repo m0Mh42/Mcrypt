0bb1231f767990f1f765303890e3c31d427cbf59
m0Mh42 <mohammadmahdi13840104@gmail.com>
2021-04-11 23:02:10 +0430

    Initial Release

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int file_length(FILE *file){
+    fseek(file, 0, SEEK_END);
+    int length = ftell(file);
+    rewind(file);
+    return length;
+}
+
+int main(int argc, char *argv[]){
+    if (argc < 4){
+        printf("Usage: ecode [to encode file] [encoded file] [password]");
+        return -1;
+    }
+    FILE *fp;
+    char *data;
+    fp = fopen(argv[1], "rb");
+    int len = file_length(fp);
+    data = malloc(len);
+    int i = 0;
+    while (!feof(fp)){
+        data[i++] = fgetc(fp);
+    }
+    fclose(fp);
+    fp = fopen(argv[2], "wb");
+    int pass = (uintptr_t)argv[3];
+    for (int j = 0; j < i-1; j++){
+        fputc((data[j] ^ pass), fp);
+    }
+    fclose(fp);
+    return 0;
+}
